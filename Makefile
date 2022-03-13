9c9ad7d4acd0dae92436ae968e19355646f8fa08
Tomi <74303735+Tomi-1997@users.noreply.github.com>
2022-03-13 16:03:26 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 #!make -f
 
-CXX=clang++-9
+CXX=clang++-9 -g3
 CXXFLAGS=-std=c++2a
 
 OBJECTS=mat.o
--- mat.cpp
+++ mat.cpp
@@ -2,9 +2,24 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
+#include <stdexcept>
 
 std::string ariel::mat(int i, int j, char c1, char c2)
 {
+	if (i <= 0 || j <= 0 || i % 2 == 0 || j % 2 == 0)
+	{
+		throw std::invalid_argument("Number exception.");
+	}
+	int c1i = c1;
+	int c2i = c2;
+	const int minValidASCII = 33;
+	const int maxValidASCII = 126;
+
+	if (c1i < minValidASCII || c1i > maxValidASCII || c2i < minValidASCII || c2i > maxValidASCII)
+	{
+		throw std::invalid_argument("Symbol exception.");
+	}
+
 	char* mat = (char*) malloc (sizeof(char) * i * j);
 	if (mat == NULL)
 	{
@@ -14,45 +29,59 @@ std::string ariel::mat(int i, int j, char c1, char c2)
 	// init matrice with c2
 	for (int k = 0; k < i*j; k++)
 	{
-		mat[k] = '|';
+		mat[k] = c2;
 	}
 	
-	// paint matrice with c1
-	for (int k = 0; k < i; k = k + 2)
+	// Edge case
+	if (i == 1 || j == 1)
 	{
-		// top and bottom
-		for (int l = k; l < i - k; l++)
+		for (int k = 0; k < i*j; k++)
 		{
-			int top = l + k*i;
-			int bot = (i*j) - top - 1;
-			mat[top] = c1;
-			mat[bot] = c1;
-		}
-
-		// left
-		for (int l = k*(i+1); l < i * (j-k); l = l + i)
-		{	
-			mat[l] = c1;
-		}
-		
-		// right
-		for (int l = (i * j) - 1 - (i*k) - k; l >= k*i - k; l = l - i)
-		{
-			mat[l] = c1;
+			mat[k] = c1;
 		}
 	}
 
-	// print matrice
+	else
+	{
+		// each time, paint a m x n matrice with only the edges
+		// next iteration paint a (m-2) x (n-2) matrice
+		// and then (m-4) x (n-4) and so on
+
+		for (int k = 0; k < i; k = k + 2)
+		{
+			// top and bottom
+			for (int l = k; l < i - k; l++)
+			{
+				int top = l + k*i;
+				int bot = (i*j) - top - 1;
+				mat[top] = c1;
+				mat[bot] = c1;
+			}
+	
+			// left
+			for (int l = k*(i+1); l < i * (j-k); l = l + i)
+			{	
+				mat[l] = c1;
+			}
+			
+			// right
+			for (int l = (i * j) - 1 - (i*k) - k; l >= k*i - k; l = l - i)
+			{
+				mat[l] = c1;
+			}
+		}	
+	}
+	std::string myRug;
+	// matrice to string
 	for (int k = 0; k < i*j; k++)
 	{
+		char current = mat[k];
 		if(k != 0 && k % i == 0)
-			std::cout << "\n";
-		std::cout << mat[k];
+		{
+			myRug += "\n";
+		}
+			myRug += current;
 	}
-
-	std::string myRug = "";
-
-
 	free(mat);
 	return myRug;
 }
